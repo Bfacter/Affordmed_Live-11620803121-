5d493864d3ab95f565e59a4fc38bde303347d83d
Bfacter <bhawanshbaleja@gmail.com>
2024-07-08 13:39:59 +0530

    Done

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ let f = document
     let b = document.getElementById("tip").value;
 
     let total = a + a * (b / 100);
-    let c = document.getElementById("totalbill");
-    c.innerHTML = `Total bill is ${total}`;
+    let c = document.getElementById("total");
+    c.innerText = `Total bill is ${total}`;
   });
